66722cd5ebac2c627ce66d67b038ad650c21388c
AbdullahAmeen <abdullah.ameenn@gmail.com>
2020-12-23 08:47:30 -0600

    new try

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let map = L.map('map').setView([32.0553, 68.0075], 13);
+
+let basemap = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+        maxZoom: 6,
+
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+			'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11'
+	}).addTo(map);
+
+
+
+let provinces = new L.GeoJSON.AJAX("data/datafile.geojson");       
+provinces.addTo(map);
